c820c117e31e72a8638e77afff409a84b6cced12
SaiGoutham-DS <gouthamsai.ds@gmail.com>
2023-03-01 11:53:12 +0530

    java Code added

---
+++ src/org/java/Projec_Java.java
@@ -0,0 +1,22 @@
+package org.java;
+
+public class Projec_Java {
+public static void main(String[] args) {
+	
+	int a = 8;
+	
+	if (a>=18) {
+		
+		System.out.println("valid");
+		
+	}
+	else {
+		System.out.println("invalid");
+	}
+	
+}
+	
+	
+	
+
+}
